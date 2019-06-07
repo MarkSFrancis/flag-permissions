682c1941e446a4c94012c04d0fc142cbb914e6ff
Mark Francis <mrkfrancis8@gmail.com>
2019-06-07 13:37:42 +0100

    Create csharp console

---
+++ csharp/Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlagRoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
